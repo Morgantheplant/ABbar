83cc538dcb07d0ab1a2997ce5fce0d1a32ccd1ed
Morgan <Morgantheplant@gmail.com>
2015-09-18 15:57:00 -0700

    remove console.logs

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,7 @@ var NavPanel = require('./NavPanel');
 var iconData = require('../constants/iconData');
 
 var App = React.createClass({
-	handler: function(){
-		console.log('this is an item')
-	},
 	render: function(){
-		console.log(iconData)
 
 		return (
 			  <NavPanel iconData={ iconData } />
