795e1b032d0d5261650b575bd85af6be013b510c
Morgan <Morgantheplant@gmail.com>
2015-09-18 15:55:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,4 @@ var React = require('react');
 
 var App = require('./src/components/App');
 
-// var TodoApp = require('./src/components/TodoApp.react');
-
 React.render(<App />, document.querySelector('#content'))
